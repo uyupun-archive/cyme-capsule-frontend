14f7f92467660432e48c278713668cb8ccc5fff7
takashi0602 <takashioicsn@gmail.com>
2019-08-26 23:14:46 +0900

    modify top page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>トップ</div>
+  <div>
+    <h1 class="c-titleTop">Cyme Capsule</h1>
+    <p class="mb-5">Cyme Capsuleとはタイムカプセルを…説明文が入ります。</p>
+    <div class="text-center mb-3">
+      <router-link to="/register" class="btn btn-lightgreen px-5"
+        >新規登録</router-link
+      >
+    </div>
+    <div class="text-center">
+      <router-link to="/login" class="btn btn-lightgreen px-5"
+        >ログイン</router-link
+      >
+    </div>
+  </div>
 </template>
