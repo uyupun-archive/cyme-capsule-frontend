68ccb4010859d7680b98eb296ec16c946c3dfd5a
takashi0602 <takashioicsn@gmail.com>
2019-09-03 04:37:46 +0900

    ログイン機能 作成

---
+++ store/user.js
@@ -0,0 +1,51 @@
+import { createNamespacedHelpers } from 'vuex'
+
+export default {
+  ...createNamespacedHelpers('user'),
+  state() {
+    return {
+      id: null,
+      accessToken: null
+    }
+  },
+  getters: {
+    userId: (state) => {
+      return state.user.id
+    }
+  },
+  mutations: {
+    setToken: (state, token) => {
+      state.accessToken = token
+    }
+  },
+  actions: {
+    async register({ commit }, { userId, password }) {
+      const userInfo = await this.$axios
+        .post(`${process.env.API_URL}user/login`, {
+          user_id: userId,
+          password
+        })
+        .catch((e) => {
+          throw e
+        })
+      if (userInfo) {
+        this.$axios.setToken(userInfo.token, 'Bearer')
+        commit('setToken', userInfo.token)
+      }
+    },
+    async login({ commit }, { userId, password }) {
+      const userInfo = await this.$axios
+        .post(`${process.env.API_URL}user/login`, {
+          user_id: userId,
+          password
+        })
+        .catch((e) => {
+          throw e
+        })
+      if (userInfo) {
+        this.$axios.setToken(userInfo.token, 'Bearer')
+        commit('setToken', userInfo.token)
+      }
+    }
+  }
+}
