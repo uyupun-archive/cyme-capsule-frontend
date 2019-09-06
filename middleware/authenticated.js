0603474f3342eeec63a02fc20b51212302411308
takashi0602 <takashioicsn@gmail.com>
2019-09-06 20:56:15 +0900

    modify authenticated

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,8 +1,9 @@
 export default function({ store, redirect }) {
-  if (
-    !store.state.user.accessToken &&
-    !localStorage.getItem('CYME_CAPSULE_TOKEN')
-  ) {
-    return redirect('/login')
+  if (!store.state.user.accessToken) {
+    if (!localStorage.getItem('CYME_CAPSULE_TOKEN')) {
+      return redirect('/login')
+    }
+    const parse = JSON.parse(localStorage.getItem('CYME_CAPSULE_TOKEN'))
+    store.commit('user/setToken', parse.user.accessToken)
   }
 }
